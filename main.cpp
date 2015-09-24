46cec008ea985afb6f11f15e9d452bfd10c2437c
xi-xi <qhn.0618@gmail.com>
2015-09-24 14:36:53 +0900

    [mod]: mod to link OpenCV libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv){
-    std::cout << "Hello, world" << std::endl;
+	cv::VideoCapture video_capture;
+	bool flg = video_capture.open("C:/Users/Takumi/Videos/ichigo.avi");
+	std::cout << flg << std::endl;
+	cv::namedWindow("TEST");
+	cv::Mat frame;
+	while (true) {
+		video_capture >> frame;
+		if (frame.empty()) {
+			break;
+		}
+		cv::imshow("TEST", frame);
+		cv::waitKey(33);
+	}
     return 0;
 }
