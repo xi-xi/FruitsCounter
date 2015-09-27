b077cea14027d661659222a90379b58dc106e44b
xi-xi <qhn.0618@gmail.com>
2015-09-27 18:05:24 +0900

    [mod]: use external path

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
 #include "TimeLapse.hpp"
 
+void showHelp() {
+	std::cout << "Usage:\n"
+		<< "\tmain <input_directory>"
+		<< std::endl;
+}
+
 int main(int argc, char** argv){
-	TimeLapse timelapse("C:/Users/Takumi/Videos/ichigo");
+	if (argc <= 1) {
+		showHelp();
+		return 1;
+	}
+	TimeLapse timelapse(argv[1]);
 	cv::namedWindow("TEST");
 	cv::Mat frame;
 	while (timelapse.isOpened()) {
 		timelapse >> frame;
 		cv::imshow("TEST", frame);
-		cv::waitKey(16);
+		cv::waitKey(33);
 	}
     return 0;
 }
