ff5481ed1ea4f112adc177a9377db1096766faf0
xi-xi <qhn.0618@gmail.com>
2015-09-27 14:17:39 +0900

    [add]: write TimeLapse skeleton

--- TimeLapse.cpp
+++ TimeLapse.cpp
@@ -1,9 +1,27 @@
 #include "TimeLapse.hpp"
 
-TimeLapse::TimeLapse(){
+#include <string>
+#include <filesystem>
+#include <opencv2/imgcodecs.hpp>
 
+TimeLapse::TimeLapse(){
+}
+
+TimeLapse::TimeLapse(const std::string& dirname) {
 }
 
 TimeLapse::~TimeLapse() {
 
+}
+
+bool TimeLapse::open(const std::string& dirname) {
+	return true;
+}
+
+bool TimeLapse::isOpened()const {
+	return true;
+}
+
+bool TimeLapse::read(cv::Mat& image) {
+	return true;
 }
--- TimeLapse.hpp
+++ TimeLapse.hpp
@@ -1,8 +1,18 @@
 #ifndef __TIMELAPASE_HPP__
 #define __TIMELAPASE_HPP__
+#include <string>
+#include <opencv2/core.hpp>
 class TimeLapse {
 public:
 	TimeLapse();
+	TimeLapse(const std::string& dirname);
 	~TimeLapse();
+	bool open(const std::string& dirname);
+	bool isOpened() const;
+	TimeLapse& operator >>(cv::Mat& image) {
+		this->read(image);
+		return *this;
+	}
+	bool read(cv::Mat& image);
 };
 #endif
