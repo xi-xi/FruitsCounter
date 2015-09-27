1beb01b3279a4f2eefbcaf9ea32c28264859ae1b
xi-xi <qhn.0618@gmail.com>
2015-09-27 14:02:40 +0900

    [add]: add TimeLapse Class

---
+++ TimeLapse.cpp
@@ -0,0 +1,9 @@
+#include "TimeLapse.hpp"
+
+TimeLapse::TimeLapse(){
+
+}
+
+TimeLapse::~TimeLapse() {
+
+}
---
+++ TimeLapse.hpp
@@ -0,0 +1,8 @@
+#ifndef __TIMELAPASE_HPP__
+#define __TIMELAPASE_HPP__
+class TimeLapse {
+public:
+	TimeLapse();
+	~TimeLapse();
+};
+#endif
