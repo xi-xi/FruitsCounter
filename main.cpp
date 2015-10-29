bee18617c7478a21f2c0d84555053c48c69b61b8
xi-xi <qhn.0618@gmail.com>
2015-10-29 11:12:34 +0900

    [mod]: mod to use dir path name

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv){
 		stream.open(argv[1]);
 	}
 	else {
-		stream.open("tomato");
+		stream.open("./tomato");
 	}
 	std::cout << "No Error" << std::endl;
 	cv::namedWindow("N");
